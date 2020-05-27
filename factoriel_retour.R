7a9fb40c8d0cd3ee947d6526364d3b89caafe6ec
jorickdefraine <defraine.jorick@gmail.com>
2020-05-27 18:38:31 +0200

    ajout du retour de la factoriel

---
+++ factoriel_retour.R
@@ -0,0 +1,12 @@
+nombre <- as.numeric(commandArgs(trailingOnly=TRUE)[1])
+
+factoriel <- function(nombre){
+  if (nombre==0){
+    return(1)
+  }
+  else{
+    factoriel(nombre-1)*nombre
+  }
+}
+
+sprintf("%.f", factoriel(nombre))
